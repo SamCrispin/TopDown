23080afb97ffcef80845705cd2db3b6ce7c2f401
Sam Crispin <samuel.crispin123@gmail.com>
2019-11-15 09:35:35 +0000

    Fixed rendering x and y directions wrong Added basic collision detection

--- scripts/Level.js
+++ scripts/Level.js
@@ -1,9 +1,10 @@
 class Level {
     constructor() {
         this.cellSize = 64;
-        this.map = levels["1"];
-        this.width = this.map.length;
-        this.height = this.map[0].length;
+        this.data = levels["1"];
+        this.width = this.data.length;
+        this.height = this.data[0].length;
+        this.size = this.width * this.height;
         this.size = this.width * this.height;
     }
 
@@ -14,8 +15,8 @@ class Level {
     initialise() {
         for (let i = 0; i < this.height; i++) {
             for (let j = 0; j < this.width; j++) {
-                let mapVal = this.map[i][j];
-                this.map[i][j] = new Cell(cells[mapVal]);
+                let mapVal = this.data[j][i];
+                this.data[j][i] = new Cell(cells[mapVal]);
             }
         }
     }
@@ -23,14 +24,14 @@ class Level {
     render() {
         for (let i = 0; i < this.height; i++) {
             for (let j = 0; j < this.width; j++) {
-                fill(level.map[i][j].col);
+                fill(this.data[i][j].col);
                 noStroke();
-                rect(i*this.cellSize, j * this.cellSize, this.cellSize, this.cellSize)
+                square(j * this.cellSize, i * this.cellSize, this.cellSize)
             }
         }
     }
 
-    static getCellAtPos(x, y) {
-        return cells[level.map[Math.floor(x / level.cellSize)][Math.floor(y / level.cellSize)]]
+    static getCellAtPos(vec) {
+        return level.data[Math.floor(vec.y / level.cellSize)][Math.floor(vec.x / level.cellSize)];
     }
 }
--- scripts/Player.js
+++ scripts/Player.js
@@ -18,29 +18,58 @@ class Player {
     }
 
     move() {
+        let xPressed = false, yPressed = false;
         this.acc.set(0);
         for (let pressed of this.keysPressed) {
             switch (pressed) {
                 case this.keys.left:
                     this.acc.x -= this.maxAcc;
+                    xPressed = true;
                     break;
                 case this.keys.right:
                     this.acc.x += this.maxAcc;
+                    xPressed = true;
                     break;
                 case this.keys.up:
                     this.acc.y -= this.maxAcc;
+                    yPressed = true;
                     break;
                 case this.keys.down:
                     this.acc.y += this.maxAcc;
+                    yPressed = true;
                     break;
             }
         }
         if (this.keysPressed.size === 0) {
-            this.acc = p5.Vector.mult(this.vel, -1).limit(this.maxAcc);
+            this.acc = p5.Vector.mult(this.vel, -1)
+        } else if (!xPressed) {
+            this.acc.x = this.vel.x * -1
+        } else if (!yPressed) {
+            this.acc.y = this.vel.y * -1
         }
+        this.acc.limit(this.maxAcc);
         this.vel.add(this.acc);
         this.vel.limit(this.maxVel);
+        this.collide();
         this.pos.add(this.vel);
+    }
+
+    collide() {
+        let v = this.pos.copy();
+        v.add(this.vel.x + this.width/2, 0);
+        if (!Level.getCellAtPos(v).traversable) this.vel.x = 0;
+
+        v = this.pos.copy();
+        v.add(this.vel.x - this.width/2, 0);
+        if (!Level.getCellAtPos(v).traversable) this.vel.x = 0;
+
+        v = this.pos.copy();
+        v.add(0, this.vel.y + this.height/2);
+        if (!Level.getCellAtPos(v).traversable) this.vel.y = 0;
+
+        v = this.pos.copy();
+        v.add(0, this.vel.y - this.height/2);
+        if (!Level.getCellAtPos(v).traversable) this.vel.y = 0;
     }
 
     keyPress() {
@@ -56,3 +85,5 @@ class Player {
         this.render();
     }
 }
+
+//TODO: circular square collision
