565539d9fe0a4eb881557f9913a96a8d40fddeed
SamCrispin <samuel.crispin@ig.com>
2020-01-15 14:50:49 +0000

    test commit

--- scripts/Cell.js
+++ scripts/Cell.js
@@ -5,4 +5,22 @@ class Cell {
         this.bgImage = bgImage;
         this.col = col;
     }
+
+    static getCorners(x, y) {
+        return {
+            topLeft: {x: x * level.cellSize, y: y * level.cellSize},
+            topRight: {x: (x + 1) * level.cellSize, y: y * level.cellSize},
+            botRight: {x: (x + 1) * level.cellSize, y: (y + 1) * level.cellSize},
+            botLeft: {x: x * level.cellSize, y: (y + 1) * level.cellSize}
+        }
+    }
+
+    static corners = {
+        topLeft: ["topRight", "botLeft"],
+        topRight: ["topLeft", "botRight"],
+        botRight: ["topRight", "botLeft"],
+        botLeft: ["topLeft", "botRight"]
+    }
 }
+
+test
